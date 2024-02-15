feed1e72ba07791091ec5d0bd48f5835a7735c37
José Divino <josedivinooficial02@gmail.com>
2024-02-15 20:24:08 -0300

    API-3: fixing RouteServiceProvider adding the constant HOME back

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
 
+    public const HOME = '';
+
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
