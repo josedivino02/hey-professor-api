a27e717b3483e12809c5e780371b6a9cd03fa3d6
José Divino <josedivinooficial02@gmail.com>
2024-02-15 23:50:30 -0300

    API-4: validation: question should be required

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -22,3 +22,19 @@ it("should be able to update a question", function () {
         'question' => 'Updating question?',
     ]);
 });
+
+describe("validation rules", function () {
+    test("question::required", function () {
+        $user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $user->id]);
+
+        Sanctum::actingAs($user);
+
+        putJson(route('questions.update', $question), [
+            'question' => '',
+        ])->assertJsonValidationErrors([
+            'question' => 'required',
+        ]);
+    });
+
+});
