66e8f9ac74c99841f41c6a68ce2026ccf38b8795
José Divino <josedivinooficial02@gmail.com>
2024-02-15 22:17:16 -0300

    API-3: refactoring response to use resource class

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Question\StoreRequest;
+use App\Http\Resources\QuestionResource;
 use App\Models\Question;
-use Symfony\Component\HttpFoundation\Response;
 
 class StoreController extends Controller
 {
@@ -17,14 +17,6 @@ class StoreController extends Controller
             'question' => $request->question,
         ]);
 
-        return response([
-            'data' => [
-                'id' => $question->id,
-                'question' => $question->question,
-                'status' => $question->status,
-                'created_at' => $question->created_at->format('Y-m-d'),
-                'updated_at' => $question->updated_at->format('Y-m-d'),
-            ],
-        ], Response::HTTP_CREATED);
+        return QuestionResource::make($question);
     }
 }
