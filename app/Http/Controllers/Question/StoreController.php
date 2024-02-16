35b9f49d2953cac59c57afb0cf87e092c3001e3f
José Divino <josedivinooficial02@gmail.com>
2024-02-15 22:00:57 -0300

    API-3: response format and status

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers\Question;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Question\StoreRequest;
 use App\Models\Question;
+use Symfony\Component\HttpFoundation\Response;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        Question::create([
+        $question = Question::create([
             'user_id' => auth()->user()->id,
             'status' => 'draft',
             'question' => $request->question,
         ]);
+
+        return response([
+            'data' => [
+                'id' => $question->id,
+                'question' => $question->question,
+                'status' => $question->status,
+                'created_at' => $question->created_at->format('Y-m-d'),
+                'updated_at' => $question->updated_at->format('Y-m-d'),
+            ],
+        ], Response::HTTP_CREATED);
     }
 }
