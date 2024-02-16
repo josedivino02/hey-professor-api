7021420b7e404d1881f6a8e9d7ae3d090b33143e
José Divino <josedivinooficial02@gmail.com>
2024-02-15 21:01:14 -0300

    API-3: [validation rules] question::min characters should be 10

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -56,7 +56,20 @@ describe("validation rules", function () {
             'question' => 'Question without a question mark',
         ]))
             ->assertJsonValidationErrors([
-                'question' => 'The :attribute should end with question mark (?).',
+                'question' => 'The question should end with question mark (?).',
+            ]);
+    });
+
+    test("question::min characters should be 10", function () {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store', [
+            'question' => 'Question?',
+        ]))
+            ->assertJsonValidationErrors([
+                'question' => 'least 10 characters',
             ]);
     });
 });
