02060059d9337b27cbf5f9fc42dcf1987a80832f
José Divino <josedivinooficial02@gmail.com>
2024-02-16 00:40:16 -0300

    API-4: testing if the endpoint is returning the correct status and format

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Models\{Question, User};
-use Laravel\Sanctum\Sanctum;
-
-use function Pest\Laravel\{assertDatabaseHas, putJson};
+use App\Models\Question;
+use App\Models\User;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\putJson;use Laravel\Sanctum\Sanctum;
 
 it("should be able to update a question", function () {
-    $user     = User::factory()->create();
+    $user = User::factory()->create();
     $question = Question::factory()->create(['user_id' => $user->id]);
 
     Sanctum::actingAs($user);
@@ -16,15 +16,15 @@ it("should be able to update a question", function () {
     ])->assertOk();
 
     assertDatabaseHas('questions', [
-        'id'       => $question->id,
-        'user_id'  => $user->id,
+        'id' => $question->id,
+        'user_id' => $user->id,
         'question' => 'Updating question?',
     ]);
 });
 
 describe("validation rules", function () {
     test("question::required", function () {
-        $user     = User::factory()->create();
+        $user = User::factory()->create();
         $question = Question::factory()->create(['user_id' => $user->id]);
 
         Sanctum::actingAs($user);
@@ -37,7 +37,7 @@ describe("validation rules", function () {
     });
 
     test("question::ending with question mark", function () {
-        $user     = User::factory()->create();
+        $user = User::factory()->create();
         $question = Question::factory()->create(['user_id' => $user->id]);
 
         Sanctum::actingAs($user);
@@ -50,7 +50,7 @@ describe("validation rules", function () {
     });
 
     test("question::min characters should be 10", function () {
-        $user     = User::factory()->create();
+        $user = User::factory()->create();
         $question = Question::factory()->create(['user_id' => $user->id]);
 
         Sanctum::actingAs($user);
@@ -66,8 +66,8 @@ describe("validation rules", function () {
         $user = User::factory()->create();
         Question::factory()->create([
             'question' => 'Lorem ipsum Divino?',
-            'status'   => 'draft',
-            'user_id'  => $user->id,
+            'status' => 'draft',
+            'user_id' => $user->id,
         ]);
 
         $question = Question::factory()->create(['user_id' => $user->id]);
@@ -82,10 +82,10 @@ describe("validation rules", function () {
     });
 
     test("question::should be unique only if id is different", function () {
-        $user     = User::factory()->create();
+        $user = User::factory()->create();
         $question = Question::factory()->create([
             'question' => 'Lorem ipsum Divino?',
-            'user_id'  => $user->id,
+            'user_id' => $user->id,
         ]);
 
         Sanctum::actingAs($user);
@@ -96,7 +96,7 @@ describe("validation rules", function () {
     });
 
     test("question::should be able to edit only if the status is in draft", function () {
-        $user     = User::factory()->create();
+        $user = User::factory()->create();
         $question = Question::factory()->create(['user_id' => $user->id, 'status' => 'published']);
 
         Sanctum::actingAs($user);
@@ -123,9 +123,36 @@ describe("security", function () {
         ])->assertForbidden();
 
         assertDatabaseHas('questions', [
-            'id'       => $question->id,
+            'id' => $question->id,
             'question' => $question->question,
         ]);
 
     });
+});
+
+test("after updating we should return a status 200 with the updated a question", function () {
+    $user = User::factory()->create();
+    $question = Question::factory()->for($user, 'user')->create();
+
+    Sanctum::actingAs($user);
+
+    $request = putJson(route('questions.update', $question), [
+        'question' => 'Lorem ipsum Divino?',
+    ])->assertOk();
+
+    $question = Question::latest()->first();
+
+    $request->assertJson([
+        'data' => [
+            'id' => $question->id,
+            'question' => $question->question,
+            'status' => $question->status,
+            'created_by' => [
+                'id' => $user->id,
+                'name' => $user->name,
+            ],
+            'created_at' => $question->created_at->format('Y-m-d h:i:s'),
+            'updated_at' => $question->updated_at->format('Y-m-d h:i:s'),
+        ],
+    ]);
 });
