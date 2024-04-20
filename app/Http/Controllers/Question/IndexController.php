b68ec53ce6d2f2326539cd782c0071e5cf946494
Divino <josedivinooficial02@gmail.com>
2024-04-20 17:14:12 -0300

    adding votes um to the list of questions

--- app/Http/Controllers/Question/IndexController.php
+++ app/Http/Controllers/Question/IndexController.php
@@ -16,6 +16,8 @@ class IndexController extends Controller
         $questions = Question::query()
         ->published()
         ->search(request()->q)
+        ->withSum('votes', 'like')
+        ->withSum('votes', 'unlike')
         ->get();
 
         return  QuestionResource::collection($questions);
