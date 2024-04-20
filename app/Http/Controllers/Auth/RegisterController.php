6bd10ff26ebf17f0d74cd8762fd6af0a5203d6f7
Divino <josedivinooficial02@gmail.com>
2024-04-19 22:01:31 -0300

    validations:name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,6 +10,10 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
-        User::create($request->all());
+        $data = request()->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        User::create($data);
     }
 }
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,7 +11,7 @@ it("Should be able to register in the application", function () {
         "name"     => "Divino",
         "email"    => "divino@divino.com",
         "password" => "password",
-    ])->assertSessionHasNoErrors();
+    ])->assertOk();
 
     assertDatabaseHas('users', [
         "name"  => "Divino",
@@ -21,4 +21,21 @@ it("Should be able to register in the application", function () {
     $divino = User::whereEmail('divino@divino.com')->first();
 
     assertTrue(Hash::check('password', $divino->password));
+});
+
+describe("validations", function () {
+
+    test('name', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['name' => $value])
+            ->assertJsonValidationErrors([
+                'name' => __(
+                    'validation.' . $rule,
+                    array_merge(['attribute' => 'name'], $meta)
+                ),
+            ]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min', 'AB', ['min' => 3]],
+        'max'      => ['max', str_repeat('*', 256), ['max' => 255]],
+    ]);
 });
